52a5bda64a7f6e72e0198b800589e728f17dee24
Barna Biro <barna.biro@lateral-inc.com>
2020-04-25 09:58:51 +0300

    Add Stock and iex API key

---
+++ config/webpack/dist/environment.prod.js
@@ -0,0 +1,1 @@
+"use strict";var _require=require("@rails/webpacker"),environment=_require.environment,webpack=require("webpack");environment.plugins.append("Provide",new webpack.ProvidePlugin({$:"jquery",jQuery:"jquery",Popper:["popper.js","default"]})),module.exports=environment;
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_160223) do
+ActiveRecord::Schema.define(version: 2020_04_25_063151) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.string "last_price"
+    t.string "decimal"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
